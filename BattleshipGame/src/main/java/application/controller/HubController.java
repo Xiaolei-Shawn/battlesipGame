8d122af03bffe6e9a00ea973ebac27c5f1eb2bb7
XiaDu <XiaDu@QPR-NB339.qpr.com>
2018-07-04 15:01:56 +0300

    Add missing controller.

--- BattleshipGame/src/main/java/application/controller/HubController.java
+++ BattleshipGame/src/main/java/application/controller/HubController.java
@@ -3,7 +3,13 @@
  */
 package application.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import application.domain.Game;
+import application.gameFactory.GameFactory;
 
 /**
  * @author XiaDu
@@ -12,5 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HubController {
 
-	
+	@RequestMapping(method = RequestMethod.GET, path = "/battleship/{level}")
+	public Game startBattleshipGame(@PathVariable("level") String level) {
+		return GameFactory.startBattleship(level);
+	}
 }
