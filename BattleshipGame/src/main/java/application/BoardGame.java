63c4fc10f879cbb6e80dd8fcca8311c9d7c96057
XiaDu <XiaDu@QPR-NB339.qpr.com>
2018-07-04 13:04:07 +0300

    Refactor project and create controller

--- BattleshipGame/src/main/java/application/BoardGame.java
+++ BattleshipGame/src/main/java/application/BoardGame.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BattleshipApplication {
+public class BoardGame {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(BattleshipApplication.class, args);
+		SpringApplication.run(BoardGame.class, args);
 	}
 
 }
---
+++ BattleshipGame/src/main/java/application/controller/Controller.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package application.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author XiaDu
+ *
+ */
+@RestController
+public class Controller {
+
+	
+}
