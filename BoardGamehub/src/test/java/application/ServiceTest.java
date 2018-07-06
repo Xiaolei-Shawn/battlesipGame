739a126ad3be1e6e2ad8462b6e7aa2c19519ebc1
shawn.xiaolei <xiaolei.shawn@gmail.com>
2018-07-06 09:44:39 +0300

    Add test sources and spring-boot-start-test dependency to pom

---
+++ BoardGamehub/src/test/java/application/ServiceTest.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package application;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author XiaDu
+ *
+ */
+@RunWith(SpringRunner.class)
+public class ServiceTest {
+
+}
