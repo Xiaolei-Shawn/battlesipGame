e3b13a35fc782bb8ebd9ec25a48190d16de96549
Xiaolei-Shawn <xiaolei.shawn@gmail.com>
2018-07-06 20:52:26 +0300

    Add level links to html and some refactorations

---
+++ BoardGamehub/src/main/resources/static/boardGameHub.js
@@ -0,0 +1,41 @@
+  const gameHub = document.getElementById("gameHub");
+  async function executeIt() {
+    const mapFunc = async (game) => {
+      
+
+      const levelItemElems = document.createElement('ul');
+
+      const gameItemElem = document.createElement('li');
+      const nameNode = document.createTextNode(game);
+      gameItemElem.appendChild(nameNode);
+
+      const lowLevelItemElem = document.createElement('li');
+      const lowLevel = document.createTextNode("Low");
+      const urlNodeLowLevel = document.createElement('a');
+      urlNodeLowLevel.setAttribute("href", "/" + game + "/low");
+      urlNodeLowLevel.appendChild(lowLevel);
+      lowLevelItemElem.appendChild(urlNodeLowLevel);
+      levelItemElems.appendChild(lowLevelItemElem);
+
+      const mediumLevelItemElem = document.createElement('li');
+      const mediumLevel = document.createTextNode("Medium");
+      const urlNodeMediumLevel = document.createElement('a');
+      urlNodeMediumLevel.setAttribute("href", "/" + game + "/medium");
+      urlNodeMediumLevel.appendChild(mediumLevel);
+      mediumLevelItemElem.appendChild(urlNodeMediumLevel);
+      levelItemElems.appendChild(mediumLevelItemElem);
+
+      const highLevelItemElem = document.createElement('li');
+      const highLevel = document.createTextNode("High");
+      const urlNodeHighLevel = document.createElement('a');
+      urlNodeHighLevel.setAttribute("href", "/" + game + "/high");
+      urlNodeHighLevel.appendChild(highLevel);
+      highLevelItemElem.appendChild(urlNodeHighLevel);
+      levelItemElems.appendChild(highLevelItemElem);
+
+      gameItemElem.appendChild(levelItemElems);
+      gameHub.appendChild(gameItemElem);
+    }
+    $.get('/', (games) => games.map(mapFunc));
+  }
+  executeIt();
--- BoardGamehub/src/test/java/application/ServiceTest.java
+++ BoardGamehub/src/test/java/application/ServiceTest.java
@@ -3,14 +3,10 @@
  */
 package application;
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
 /**
  * @author XiaDu
  *
  */
-@RunWith(SpringRunner.class)
 public class ServiceTest {
 
 }
